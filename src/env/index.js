10f3609b8055b59bafd8cce9a9c01f27865a6251
ngot23 <levuquang2k1@gmail.com>
2021-11-29 09:27:54 +0700

    (+) clean

--- src/env/index.js
+++ src/env/index.js
@@ -2,4 +2,4 @@ export const ISSUERADDR = '0xE44CFe606eE964c99824129619af116e2cd4AA9a'
 export const CONTRACTADDR = '0x9Cc365bDE134709379ED4aA3A96B6f281b95cc9D'
 export const AES192KEY = '0xc4682bcd4430f2a6d4d84b7654be7d0cca5b6d7421f3c133'
 // convert to passphrase -> change 256 key smart contract -> string instead of bytes24
-export const IPFSAPI = 'http://localhost:5001'
+export const IPFSAPI = 'http://127.0.0.1:5001'
